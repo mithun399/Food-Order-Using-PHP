bf1fb4201d291c11080290f64bf8405a9176fe55
mithun399 <msovik399@gmail.com>
2022-05-16 15:37:17 +0600

    add admin

--- admin/admin.php
+++ admin/admin.php
@@ -4,6 +4,39 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Admin</h1>
+            <br><br>
+            <a class="btn-primary" style="color:white;" href="add_admin.php">Add Admin</a>
+            <br><br>
+            <table class="tbl-full">
+                <tr>
+                    <th>Sl NO</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Mithun Sutradhar</td>
+                    <td>mithun399</td>
+                    <td>
+                        <a href="" class="btn-secondary">edit</a>
+                        <a href="" class="btn-danger">delete</a>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Mithun Sutradhar</td>
+                    <td>mithun399</td>
+                    <td>edit/delete</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Mithun Sutradhar</td>
+                    <td>mithun399</td>
+                    <td>edit/delete</td>
+                </tr>
+            </table>
         </div>
 
     </div>
--- admin/partial/menu.php
+++ admin/partial/menu.php
@@ -1,3 +1,5 @@
+<?php include('../config/constant.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
