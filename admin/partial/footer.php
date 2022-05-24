a1b090f9b68fbf3418093200348256f7362112e6
mithun399 <msovik399@gmail.com>
2022-05-24 10:54:39 +0600

    admin panel complete

--- admin/partial/footer.php
+++ admin/partial/footer.php
@@ -1,11 +1,10 @@
- <!-- Footer Section Start -->
- <div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2022 all rights reserved, <a href="https://github.com/mithun399">Mithun Sutradhar</a>
-            </p>
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Vijay Thapa</a></p>
+            </div>
         </div>
-    </div>
-    <!-- Footer Section End -->
-</body>
+        <!-- Footer Section Ends -->
 
+    </body>
 </html>
--- admin/partial/menu.php
+++ admin/partial/menu.php
@@ -1,28 +1,30 @@
-<?php include('../config/constant.php'); ?>
+<?php 
 
-<!DOCTYPE html>
-<html lang="en">
+    include('../config/constants.php'); 
+    include('login-check.php');
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Food-Order Home Page</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
+?>
 
-<body>
-    <!-- Menu Section Start -->
-    <div class="menu">
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="admin.php">Admin</a></li>
-                <li><a href="category.php">Category</a></li>
-                <li><a href="food.php">Food</a></li>
-                <li><a href="order.php">Order</a></li>
-            </ul>
+
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="manage-category.php">Category</a></li>
+                    <li><a href="manage-food.php">Food</a></li>
+                    <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
         </div>
-
-    </div>
-    <!-- Menu Section End -->
+        <!-- Menu Section Ends -->
